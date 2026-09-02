5aa893889f1af569f8eaae78409bc6a32eb70599
TimSwettlen <tim.swettlen@colorado.edu>
2026-09-02 07:42:26 +0500

    Add files via upload

--- site-config.js
+++ site-config.js
@@ -2,8 +2,8 @@ window.SPEAKER_SERIES_CONFIG = {
   mainRegistrationUrl: "",
   registrationUrls: {
     marvell: "https://events.vtools.ieee.org/m/573685",
-    intel: "",
-    panasonic: "",
+    intel: "https://events.vtools.ieee.org/m/575064",
+    panasonic: "https://events.vtools.ieee.org/m/575322",
     student: ""
   },
   hsdProgramUrl: "https://www.colorado.edu/ecee/academics/graduate-programs/professional-masters/high-speed-digital-engineering",
